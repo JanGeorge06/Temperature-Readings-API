dda69125bc5d1bef91052b3aaeb3581866379ae7
Jan <janbernaba@hotmail.com>
2022-01-03 16:36:19 +0200

    Firestore REST api

--- main.py
+++ main.py
@@ -1,74 +1,83 @@
-from flask import Flask, request
+from datetime import datetime
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 app = Flask(__name__)
 
-data = {
+cred = credentials.Certificate('iotproject-f3325-firebase-adminsdk-ozxun-9093ec310e.json')
+firebase_admin.initialize_app(cred)
 
-}
-
-
-@app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-    return data.get(id, "Not Found")
+db = firestore.client()
 
 
-@app.route('/users', methods=['GET'])
+@app.route('/')
+def hello():
+    return "REST API for Firestore \n Made with Flask"
+
+
+# Get single room
+@app.route('/rooms/<int:room>', methods=['GET'])
+def get_temp(room):
+    rooms = db.collection('room' + str(room))
+    docs = rooms.stream()
+    data = []
+    for doc in docs:
+        temp = str(doc.to_dict()['temp'])
+        date = str(doc.to_dict()['date'])
+        data.append({'temp': temp, 'date': date})
+
+    return jsonify(data)
+
+
+# Get all rooms data
+@app.route('/rooms', methods=['GET'])
 def get_users():
-    return data
+    room1 = db.collection('room1')
+    room2 = db.collection('room2')
+    room3 = db.collection('room3')
+    docs1 = room1.stream()
+    docs2 = room2.stream()
+    docs3 = room3.stream()
+    data1 = []
+    data2 = []
+    data3 = []
+    for doc in docs1:
+        temp = str(doc.to_dict()['temp'])
+        date = str(doc.to_dict()['date'])
+        data1.append({'temp': temp, 'date': date})
+    for doc in docs2:
+        temp = str(doc.to_dict()['temp'])
+        date = str(doc.to_dict()['date'])
+        data2.append({'temp': temp, 'date': date})
+    for doc in docs3:
+        temp = str(doc.to_dict()['temp'])
+        date = str(doc.to_dict()['date'])
+        data3.append({'temp': temp, 'date': date})
+
+    dic = {
+        "Room1": data1,
+        "Room2": data2,
+        "Room3": data3
+    }
+    return jsonify(dic)
 
 
-@app.route('/login', methods=['POST'])
-def sign_in():
+@app.route('/add', methods=['POST'])
+def add_document():
     body = request.json
-    if body['username'] == "":
-        return "Enter a username"
-    elif not checkIfUsernameExists(body['username']):
-        return "Username does not exist"
-    elif not checkIfPasswordCorrectMatches(body['username']):
-        return "Password is incorrect"
+    temp = body['temp']
+    room = body['room']
+    if temp == "" or room == "":
+        return 'Please pass values'
     else:
-        return "Logged in successfully"
-
-
-@app.route('/signup', methods=['POST'])
-def sign_up():
-    body = request.json
-    if body['username'] == "" or body['password'] == "":
-        return "Please fill fields"
-    if checkIfUsernameExists(body['username']):
-        return "Username already exists"
-    else:
-        if not bool(data):
-            data[1] = body
-            return "Signed up!"
-        else:
-            data[list(data.keys())[-1] + 1] = body
-            return "Signed up!"
-
-
-def checkIfUsernameExists(username):
-    usernames = []
-    i = 1
-    for user in data:
-        usernames.append(data.get(i)['username'])
-        i = i + 1
-    if username in usernames:
-        return True
-    else:
-        return False
-
-
-def checkIfPasswordCorrectMatches(username):
-    body = request.json
-    password = body['password']
-    data_tolist = list(data.values())
-    print(data_tolist)
-    user_pass = {}
-    i = 0
-    for user in data_tolist:
-        user_pass[data_tolist[i]['username']] = data_tolist[i]['password']
-        i = i + 1
-    if user_pass[username] != password:
-        return False
-    else:
-        return True
+        doc = db.collection(room).document()
+        date = datetime.now()
+        doc.set(
+            {
+                "temp": temp,
+                "date": date
+            }
+        )
+        return "Document created successfully at " + str(date)
